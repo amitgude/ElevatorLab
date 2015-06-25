1d4773a42d49bb76d8d1bd77ecd080b0b178d9d5
amitgude <gudeamit148@gmail.com>
2015-06-25 16:57:36 -0700

    Added Comments

--- Elevator/src/Floor.java
+++ Elevator/src/Floor.java
@@ -1,13 +1,15 @@
 
 public class Floor {
    
-	// current floor Number
+	// current floor Number attribute
 	private Integer currentFloorNumber;
 
+	// getter function for CurrentFLoorNumber
 	public Integer getCurrentFloorNumber() {
 		return currentFloorNumber;
 	}
 
+	// setter function for CurrentFloorNumber
 	public void setCurrentFloorNumber(Integer currentFloorNumber) {
 		this.currentFloorNumber = currentFloorNumber;
 	} 
--- Elevator/src/FloorButton.java
+++ Elevator/src/FloorButton.java
@@ -1,17 +1,20 @@
 
 public class FloorButton {
 
+	//  attribute to store selected floor
 	private Integer value;
 
+	// getter function for value attribute
 	public Integer getValue() {
 		return value;
 	}
 
+	 // setter function for value attribute
 	public void setValue(Integer value) {
 		this.value = value;
 	}
 	
-	
+	// function to set the entered floor Number to floorNUmber
 	public void click( Integer floorNumber){
 		setValue(floorNumber);
     }
