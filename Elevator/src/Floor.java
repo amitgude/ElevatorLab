49194a917ce399a9d578e5a76d67cbaab56958a8
amitgude <gudeamit148@gmail.com>
2015-06-25 16:31:09 -0700

    Added currentFloorNumber attribute

--- Elevator/src/Floor.java
+++ Elevator/src/Floor.java
@@ -1,4 +1,13 @@
 
 public class Floor {
 
+	private Integer currentFloorNumber;
+
+	public Integer getCurrentFloorNumber() {
+		return currentFloorNumber;
+	}
+
+	public void setCurrentFloorNumber(Integer currentFloorNumber) {
+		this.currentFloorNumber = currentFloorNumber;
+	} 
 }
