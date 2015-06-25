595780d7a55a3fb213af875f1a03ad151cc5da7d
rahuldcosta <rahuldc999@gmail.com>
2015-06-25 16:11:57 +0530

    elevator door

---
+++ Elevator/src/elevatordoor.java
@@ -0,0 +1,12 @@
+
+public class elevatordoor {
+
+	String color;
+	Boolean estatus;
+	public elevatordoor() {
+		this.color="brown";
+		this.estatus=false;
+		// TODO Auto-generated constructor stub
+	}
+
+}
