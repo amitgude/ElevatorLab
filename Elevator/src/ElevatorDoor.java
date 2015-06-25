00d2f2b66261595854fd5090b07e4b5e968fa48d
rahuldcosta <rahuldc999@gmail.com>
2015-06-25 16:30:38 +0530

    the elevatordoor done

--- Elevator/src/ElevatorDoor.java
+++ Elevator/src/ElevatorDoor.java
@@ -9,5 +9,18 @@ public class ElevatorDoor {
 		this.estatus=false;
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	public void opendoor(){
+		
+		this.estatus=true;
+		System.out.println("Door Opening ...............");
+	}
+	
+public void closedoor(){
+		
+		this.estatus=false;
+		System.out.println("Door Closing ...............");
+	}
 
 }
