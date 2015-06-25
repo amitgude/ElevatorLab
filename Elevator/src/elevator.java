f52e165ea56274b44758bea7cf7a56eb5bbbebed
nandesh <nnnaik92@gmail.com>
2015-06-25 15:56:57 -0700

    by nan

---
+++ Elevator/src/elevator.java
@@ -0,0 +1,15 @@
+
+public class elevator {
+     int current_pos ;
+
+	public int getCurrent_pos() {
+		return current_pos;
+	}
+
+	public void setCurrent_pos(int current_pos) {
+		this.current_pos = current_pos;
+	} 
+     
+	
+	
+}
