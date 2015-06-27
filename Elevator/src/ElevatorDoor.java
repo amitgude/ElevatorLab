947228535d4cd11034d91ff66447a07b74a9bc20
amitgude <gudeamit148@gmail.com>
2015-06-27 16:43:21 -0700

    Added comments

--- Elevator/src/ElevatorDoor.java
+++ Elevator/src/ElevatorDoor.java
@@ -1,24 +1,24 @@
 
 public class ElevatorDoor {
-  // E;evator attributes
+  // Elevator attributes
 	String color;
 	Boolean estatus;
 	
+	//Elevator door constructor
 	public ElevatorDoor() {
 		this.color="brown";
 		this.estatus=false;
 		// TODO Auto-generated constructor stub
 	}
 	
-	
+	// Function to simulate opening of the elevator door
 	public void opendoor(){
-		
 		this.estatus=true;
 		System.out.println("Door Opening ...............");
 	}
 	
-public void closedoor(){
-		
+	// Function to simulate closing of the elevator door
+	public void closedoor(){
 		this.estatus=false;
 		System.out.println("Door Closing ...............");
 	}
