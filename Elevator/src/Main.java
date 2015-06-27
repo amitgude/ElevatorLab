048bf34fda6e2274442f1f44db8514bd1005d141
amitgude <gudeamit148@gmail.com>
2015-06-27 16:41:32 -0700

    added display method added user input for floor number

--- Elevator/src/Main.java
+++ Elevator/src/Main.java
@@ -1,27 +1,27 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Elevator elevator = new Elevator() ; 
+		Floor f = new Floor();
+		FloorButton Fb = new FloorButton()	;  
+		Scanner scan = new Scanner(System.in);
+        Integer floorNumber;
+        
+		elevator.display();
 		
+		floorNumber = scan.nextInt();
 		
-		     Floor f = new Floor();
-		
-		    
-		     
-		     
-		     FloorButton Fb = new FloorButton()	;  
-		     
-		     
-		     Fb.setValue(-1);
-		     
-		
-	        Elevator e = new Elevator() ; 
-	        e.change(f, Fb);
-		
-		
-		
+		Fb.setValue(floorNumber);
+
+		elevator.change(f, Fb);
+
+
+
 	}
 
 }
