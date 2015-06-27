697fdac196835cb729f125a494e23728b2412cba
amitgude <gudeamit148@gmail.com>
2015-06-27 16:36:35 -0700

    Added display function to display floor button values

--- Elevator/src/Elevator.java
+++ Elevator/src/Elevator.java
@@ -1,6 +1,13 @@
 
 public class Elevator {
    private ElevatorDoor door = new ElevatorDoor(); 
+   
+   
+   public void display(){
+	   System.out.println("Select a Floor");
+	   System.out.println("1 2 3 4 5");
+	   System.out.println("6 7 8 9");
+   }
 	 
 	//this method for changing the floor
 	public void change(Floor f ,  FloorButton Fb)
