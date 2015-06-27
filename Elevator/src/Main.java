af322bd4a4d455960d013320d0c7a495406c71c0
amitgude <gudeamit148@gmail.com>
2015-06-27 16:59:20 -0700

    Version 2

--- Elevator/src/Main.java
+++ Elevator/src/Main.java
@@ -5,13 +5,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ 
+	// Object Initialization
 		Elevator elevator = new Elevator() ; 
 		Floor f = new Floor();
 		FloorButton Fb = new FloorButton()	;  
 		Scanner scan = new Scanner(System.in);
         Integer floorNumber;
-        
+     
+      
 		elevator.display();
 		
 		floorNumber = scan.nextInt();
